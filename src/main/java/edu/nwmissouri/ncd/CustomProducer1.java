0b7531ccd12f8863b6c993e33858c5830d947d8a
Saroj Paudel <sp.saroj53@gmail.com>
2020-10-04 12:10:51 -0500

    created custom producer 1 and tested

---
+++ src/main/java/edu/nwmissouri/ncd/CustomProducer1.java
@@ -0,0 +1,101 @@
+package edu.nwmissouri.ncd;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Scanner;
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+ 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+
+/**
+ * Created by sunilpatil on 12/28/15. Modified by Saroj Paudel on 10/04/2020  .
+ */
+public class CustomProducer1 {
+  private static Scanner in;
+  private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+  public static void main(String[] argv) throws Exception {
+    if (argv.length != 1) {
+      System.err.println("Please specify 1 parameter (the name of the topic)");
+      System.exit(-1);
+    }
+    String topicName = argv[0];
+    in = new Scanner(System.in);
+    System.out.println("Thank you for providing the topic " + topicName + "\n");
+    System.out.println("Enter message (type exit to quit).\n");
+
+    // Configure the Producer
+    Properties configProperties = new Properties();
+    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.ByteArraySerializer");
+    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    System.out.println("The configuration properties are: " + configProperties.toString());
+    System.out.println("\nWill use this configuration to create a producer.\n");
+
+    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+
+    // Custom Producer
+    String mySecretKey = "thisisthesecretkey";
+    String line = in.nextLine();
+    while (!line.equals("exit")) {
+      ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, encrypt(line, mySecretKey));   //call encryption method
+      producer.send(rec);
+      line = in.nextLine();
+    }
+
+    in.close();
+    producer.close();
+
+  }
+
+  public static void setKey(String myKey) 
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16); 
+            secretKey = new SecretKeySpec(key, "AES");
+        } 
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } 
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public static String encrypt(String strToEncrypt, String secret) 
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+}
+
